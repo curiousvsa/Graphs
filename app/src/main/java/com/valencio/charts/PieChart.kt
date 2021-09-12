a44d61d8dc713ee70863e9c4a6cf16460493af7e
vsa15 <valencioaraujo15@gmail.com>
2021-09-12 17:05:49 +0530

    --- PieChart Screen And Functionality ---

--- app/src/main/java/com/valencio/charts/PieChart.kt
+++ app/src/main/java/com/valencio/charts/PieChart.kt
@@ -3,6 +3,7 @@ package com.valencio.charts
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.valencio.lib.PieChart
 import com.valencio.lib.PieChartBean
 import java.math.BigDecimal
 
@@ -10,9 +11,11 @@ class PieChart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pie_chart)
+        setChartData()
     }
 
-    /*private fun setChartData() {
+    private fun setChartData() {
+        val pieChartView = findViewById<PieChart>(R.id.pieChartView)
         val list = arrayListOf<PieChartBean>()
         list.add(PieChartBean("1", BigDecimal(30), Color.BLUE))
         list.add(PieChartBean("2", BigDecimal(22), Color.GREEN))
@@ -20,6 +23,6 @@ class PieChart : AppCompatActivity() {
         list.add(PieChartBean("4", BigDecimal(15), Color.CYAN))
         list.add(PieChartBean("5", BigDecimal(7), Color.MAGENTA))
         list.add(PieChartBean("6", BigDecimal(5), Color.DKGRAY))
-        pieChartView.mData = list
-    }*/
+        pieChartView!!.mData = list
+    }
 }
